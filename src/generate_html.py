d711bcfaf88fb6c8bb6fff508b904f39bdb30e29
Erfan Ghofrani <erfan.ghofrani.ch@gmail.com>
2025-05-03 12:19:56 +0330

    fix final

--- src/generate_html.py
+++ src/generate_html.py
@@ -1,14 +1,14 @@
 import os
 
 # Create docs directory in the root
-os.makedirs('../docs', exist_ok=True)
+os.makedirs('docs', exist_ok=True)
 
 # Read names from src/names.txt
 with open('src/names.txt', 'r') as f:
     names = f.readlines()
 
 # Generate index.html in docs
-with open('../docs/index.html', 'w') as f:
+with open('docs/index.html', 'w') as f:
     f.write('<html><body><h1>Names List</h1><ul>\n')
     for name in names:
         f.write(f'<li>{name.strip()}</li>\n')
